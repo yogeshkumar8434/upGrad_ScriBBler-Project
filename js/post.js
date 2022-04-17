70f41a7862ac64b6b785f41cf6be4dfe72621703
yogeshkumar8434 <65352329+yogeshkumar8434@users.noreply.github.com>
2022-04-17 23:41:52 +0530

    Update post.js

--- js/post.js
+++ js/post.js
@@ -1,22 +1,3 @@
-function addcmt(id){
-    var addEventName = id.value +'<br>';
-    var a = document.getElementById('commentBox');
-    document.getElementById('comments-wrapper').innerHTML += '<p>'+addEventName+'</p>';          
-    a.value=a.defaultValue;
-}
-
-
-var counter = 0;
-function LikeButton(){
-    document.getElementById('liked').innerHTML="Liked!";
-    counter++;
-    document.getElementById('likeText').innerHTML = counter+" people have like this!"
-}
-
-
-
-
-
 var titleEle = document.getElementById("postTitle");
 var contentEle = document.getElementById("postContent");
 var authorEle = document.getElementById("author");
