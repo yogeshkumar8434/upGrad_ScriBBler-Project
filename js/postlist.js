aab0a74d02d08bb869d2091bf868292e9822cbaa
yogeshkumar8434 <65352329+yogeshkumar8434@users.noreply.github.com>
2022-04-17 23:42:12 +0530

    All Are Done!

--- js/postlist.js
+++ js/postlist.js
@@ -1,70 +1,55 @@
+var deletePostModal = document.getElementById("deletePostmodal");
+var cancelDelete = document.getElementById("cancelDelete");
+var confirmDelete = document.getElementById("confirmDelete");
 
-var selectname = document.getElementById('uname');
-function extand(){
-    location.href = "../html/post.html";
-    let an=document.getElementById('author').innerHTML=selectname;
-    // console.log(an)
+// Get the button that opens the modal
+var deleteIcons = document.querySelectorAll(".delete-icon");
+var deleteIconsArray = Array.from(deleteIcons);
 
-}   
+// Get the dot icons
+var dots = document.querySelectorAll(".dots");
+var dotsArray = Array.from(dots);
 
+// Get the No button that closes the modal
+// var closeDeletePostModal = document.getElementById("closeDeletePostModal");
+var selectedPost;
+openDeletePostModal = (deletePostModal, clickedIcon) => {
+  selectedPost = "";
+  openModal(deletePostModal);
+  selectedPost = clickedIcon.closest(".post");
+};
 
-const trashBtn = document.getElementsByClassName('delete-icon');
-const trashModal = document.getElementById('signInModal');
+deletePost = () => {
+  selectedPost.remove();
+  closeModal(deletePostModal);
+};
 
-const noBtn = document.getElementById('noBtn');
-
-
-
-for (const ele of trashBtn) {
-    ele.addEventListener('click', () => {
-
-        trashModal.style.display = 'block';
-
-    });
-}
-
-noBtn.addEventListener('click', () => {
-    trashModal.style.display = 'none';
+// Updating Session storage before redirecting to post.html page to view the clicked post
+navigateToPost = postThread => {
+  var post = postThread.closest(".post");
+  var author = post.querySelector(".username p").textContent.trim();
+  var postTitle = post.querySelector(".title p").textContent.trim();
+  var postContent = post.querySelector(".content").textContent.trim();
+  sessionStorage.setItem("author", author);
+  sessionStorage.setItem("postTitle", postTitle);
+  sessionStorage.setItem("postContent", postContent);
+  window.location.href = "../html/post.html";
+};
+// Event Listeners
+deleteIconsArray.map(deleteIcon => {
+  deleteIcon.addEventListener("click", e =>
+    openDeletePostModal(deletePostModal, e.target)
+  );
+});
+dotsArray.map(dot => {
+  dot.addEventListener("click", e => navigateToPost(e.target));
 });
 
+window.addEventListener("click", function(event) {
+  if (event.target == deletePostModal) {
+    deletePostModal.style.display = "none";
+  }
+});
 
-
-// var deletePostModal = document.getElementById("deletePostmodal");
-// var cancelDelete = document.getElementById("cancelDelete");
-// var confirmDelete = document.getElementById("confirmDelete");
-// var selectedPost;
-// openDeletePostModal = (deletePostModal, clickedIcon) => {
-//   selectedPost = "";
-//   openModal(deletePostModal);
-//   selectedPost = clickedIcon.closest(".post");
-// };
-
-navigateToPost = postThread => {
-    var post = postThread.closest(".post");
-    var author = post.querySelector(".username p").textContent.trim();
-    var postTitle = post.querySelector(".title p").textContent.trim();
-    var postContent = post.querySelector(".content").textContent.trim();
-    sessionStorage.setItem("author", author);
-    sessionStorage.setItem("postTitle", postTitle);
-    sessionStorage.setItem("postContent", postContent);
-    window.location.href = "../html/post.html";
-  };
-  // Event Listeners
-  deleteIconsArray.map(deleteIcon => {
-    deleteIcon.addEventListener("click", e =>
-      openDeletePostModal(deletePostModal, e.target)
-    );
-  });
-  dotsArray.map(dot => {
-    dot.addEventListener("click", e => navigateToPost(e.target));
-  });
-  
-  window.addEventListener("click", function(event) {
-    if (event.target == deletePostModal) {
-      deletePostModal.style.display = "none";
-    }
-  });
-  
-  cancelDelete.addEventListener("click", () => closeModal(deletePostModal));
-  confirmDelete.addEventListener("click", () => deletePost());
-  
+cancelDelete.addEventListener("click", () => closeModal(deletePostModal));
+confirmDelete.addEventListener("click", () => deletePost());
